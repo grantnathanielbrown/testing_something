9fd86170e8dff557320eecd10f530323ba2d0361
Grant Brown <grant@Grants-MacBook-Pro.local>
2021-06-24 08:26:20 -0400

    this is going to be rejected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react'
-import Winsome from './Winsome'
+import React, { Component } from 'react';
+
 export default class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {
 
-}
+    }
+  }
   render() {
+
     return (
       <div>
         
+        <a href="#">hello I am the change on feature a that is going to be rejected</a>
       </div>
     )
   }
 }
+
+
 
