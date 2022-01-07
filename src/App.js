bc9c78cf6a9c41aaf34bfa20cfe447fcee9f84ec
Grant Brown <grant@ip-10-0-0-2.ec2.internal>
2022-01-07 13:53:37 -0500

    before modifying gitignore.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
-import Winsome from './Winsome'
+import React, { Component } from 'react';
+import "./App.css";
+import Timeout from './components/Timeout';
+import UEQueue from './components/UEQueue';
+
 export default class App extends Component {
+  
   constructor(props) {
     super(props)
 
-}
+    }
+
+  
+
   render() {
     return (
-      <div>
-        
+      <div className='app'>
+        {/* <UEQueue></UEQueue> */}
+        <Timeout></Timeout>
       </div>
     )
   }
