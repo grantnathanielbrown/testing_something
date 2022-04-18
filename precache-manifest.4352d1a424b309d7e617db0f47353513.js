b81e61597afb0ac1608a5c2fed95aa07d24d9a44
Grant Brown <grant@Grants-MacBook-Pro.local>
2022-04-17 20:48:22 -0400

    Updates

---
+++ precache-manifest.4352d1a424b309d7e617db0f47353513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328164c4960a61c05b79f92c7736dfe6",
+    "url": "/testing_something/index.html"
+  },
+  {
+    "revision": "1c4144ce2ec0d84940bd",
+    "url": "/testing_something/static/css/main.5a2867f9.chunk.css"
+  },
+  {
+    "revision": "42e729c086d1afffffaf",
+    "url": "/testing_something/static/js/2.fea79f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing_something/static/js/2.fea79f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4144ce2ec0d84940bd",
+    "url": "/testing_something/static/js/main.11bd89c4.chunk.js"
+  },
+  {
+    "revision": "ccc81d8602c53e55193c",
+    "url": "/testing_something/static/js/runtime-main.7a8d32ee.js"
+  }
+]);
