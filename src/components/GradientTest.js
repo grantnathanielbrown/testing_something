efedfc62546688476c911ae5593afa6888cdc15f
Grant Brown <grant@Grants-MacBook-Pro.local>
2022-04-17 22:10:59 -0400

    build

--- src/components/GradientTest.js
+++ src/components/GradientTest.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function GradientTest() {
   return (
-    <div className="h-96 w-96 text-white bg-gradient-to-r from-blue-900 to-blue-700">
-        <p>This is a test sentence.</p>
+    <div className="h-full w-full text-white bg-gradient-to-r from-blue-900 to-blue-700">
+                  <p>This is a test sentence.</p>
+        <div className="absolute opacity-5 h-full w-full bg-red-800">
+
+        </div>
     </div>
   )
 }
+
